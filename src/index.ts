997d3dee3861ca8f05efe58058df100558776073
Barnaby Thomas <barnabythomas@Barnabys-MacBook-Pro.local>
2026-07-14 13:48:21 +0100

    adding login function

--- src/index.ts
+++ src/index.ts
@@ -91,6 +91,38 @@ app.post('/register', async (req, res) => {
     }
 })
 
+app.post('/login', async (req, res) => {
+
+    try{
+        const {email, password} = req.body;
+        
+        //finding user account
+        const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        if (result.rows.length === 0) {
+            return res.status(401).json({ error: 'Email not found' });
+        }
+        
+        //checking whether passwords match
+        const user = result.rows[0];
+        const passwordMatch = await bcrypt.compare(password, user.password_hash);
+        if (!passwordMatch) {
+            return res.status(401).json({ error: 'Wrong password' });
+        }
+
+        const token = jwt.sign(
+            { userId: user.id },
+            process.env.JWT_SECRET || '',
+            { expiresIn: '7d' }
+        );
+        
+        res.json({ token });
+
+    } catch(error) {
+        console.error(error);
+        res.status(500).json({ error: 'Login failed'})
+    }
+})
+
 pool.query('SELECT NOW()', (err: Error | null, res: any) => {
     if (err) {
         console.error('Database connection error:', err);
